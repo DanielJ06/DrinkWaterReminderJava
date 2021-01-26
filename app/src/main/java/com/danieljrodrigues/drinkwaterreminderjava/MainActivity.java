f7ab7bc081ab9f5fab90978c8f953df9755eb23b
danieljesus <daniel.j.rodrigues06@gmail.com>
2021-01-26 17:37:12 -0300

    Set 24 hours time picker

--- app/src/main/java/com/danieljrodrigues/drinkwaterreminderjava/MainActivity.java
+++ app/src/main/java/com/danieljrodrigues/drinkwaterreminderjava/MainActivity.java
@@ -3,12 +3,25 @@ package com.danieljrodrigues.drinkwaterreminderjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TimePicker timePicker;
+    private Button notifyBtn;
+    private EditText intervalInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timePicker = findViewById(R.id.timePicker);
+        notifyBtn = findViewById(R.id.notiyBtn);
+        intervalInput = findViewById(R.id.textNumberInterval);
+
+        timePicker.setIs24HourView(true);
     }
 }
